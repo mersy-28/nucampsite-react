cd012d7c5d5db6d138264f8b2f763db2139b8620
Emaan Hookey <em.grey28@gmail.com>
2025-08-10 20:00:12 -0230

    workshop tasks completed

---
+++ src/features/campsites/CampsitesList.js
@@ -0,0 +1,19 @@
+import CampsiteCard from "./CampsiteCard";
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { Row, Col } from 'reactstrap';
+
+const CampsitesList = () => {
+  return (
+    <Row className="ms-auto">
+      {CAMPSITES.map((campsite) => {
+        return (
+          <Col md="5" className="m-4" key={campsite.id}>
+            <CampsiteCard campsite={campsite} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
+}
+
+export default CampsitesList;

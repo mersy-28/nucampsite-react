427b201df8ca0c2b321e4021fc1257453a15de1e
Emaan Hookey <em.grey28@gmail.com>
2025-08-13 22:13:39 -0230

    campsitesSlice

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,11 +1,13 @@
 import CampsiteCard from "./CampsiteCard";
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { Row, Col } from 'reactstrap';
+import { selectAllCampsites } from "./campsitesSlice";
 
 const CampsitesList = () => {
+  const campsites = selectAllCampsites();
+
   return (
     <Row className="ms-auto">
-      {CAMPSITES.map((campsite) => {
+      {campsites.map((campsite) => {
         return (
           <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />

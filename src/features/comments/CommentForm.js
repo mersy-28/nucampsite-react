d58ac7fedc2399b87303aca6264f8d81822bda7e
Emaan Hookey <em.grey28@gmail.com>
2025-08-25 19:23:53 -0230

    Week 3 Workshop

---
+++ src/features/comments/CommentForm.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { validateCommentForm } from '../../utils/validateCommentForm';
+
+function CommentForm({ campsiteId }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleSubmit = (values) => {
+    const comment = {
+      id: values.id,
+      campsiteId: parseInt(campsiteId),
+      rating: parseInt(values.rating),
+      text: values.commentText,
+      author: values.author
+    };
+    console.log(comment);
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button
+        outline
+        onClick={() => setModalOpen(true)}
+      >
+        <i className='fa fa-pencil fa-lg' /> Add Comment
+      </Button>
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setModalOpen(false)}>
+          Add Comment
+        </ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{
+              rating: undefined,
+              author: '',
+              commentText: ''
+            }}
+            onSubmit={handleSubmit}
+            validate={validateCommentForm}
+          >
+            <Form>
+
+              {/* rating */}
+              <FormGroup>
+                <Label htmlFor='rating'>Rating</Label>
+                <Field
+                  name='rating'
+                  as='select'
+                  className='form-control'
+                >
+                  <option>Select</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Field>
+                <ErrorMessage name='rating'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
+
+              {/* author. */}
+              <FormGroup>
+                <Label htmlFor='author'>Your Name</Label>
+                <Field
+                  name='author'
+                  placeholder='Your Name'
+                  className='form-control'
+                />
+                <ErrorMessage name='author'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
+
+              {/* comment */}
+              <FormGroup>
+                <Label htmlFor='commentText'>Comment</Label>
+                <Field
+                  name='commentText'
+                  as='textarea'
+                  rows='12'
+                  className='form-control'
+                />
+              </FormGroup>
+
+              <Button type='submit' color='primary'>
+                Submit
+              </Button>
+
+            </Form>  
+          </Formik>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+}
+
+export default CommentForm;
